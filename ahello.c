60f79b6425b0ae82027f7f47a0b5935478dfdb06
local <local@machine>
2026-09-25 11:49:00 -0500

    initial commit

---
+++ ahello.c
@@ -0,0 +1,14 @@
+// c test program v2, inlcuding argument
+
+#include <stdio.h>
+
+int main(int argc, char *argv[]) {
+	if (argc < 2) {	
+		printf("Usage: %s <name>\n", argv[0]);
+		return 1;
+	}
+
+	printf("Hello, %s!\n", argv[1]);
+	return 0;
+}
+
