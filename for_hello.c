f5079564d0eec75e9159f56bbc3397bfeab8a393
local <local@machine>
2026-09-25 12:05:27 -0500

    After completing first build of this test tool

---
+++ for_hello.c
@@ -0,0 +1,15 @@
+//This a test c program to print 10x hello messages using a for loop to use to practice system and low level tools for security training
+
+#include <stdio.h>
+
+int main()
+{
+    int i;
+
+    for(i=0; i < 10; i++)
+    {
+        puts("Hello, world!\n");
+    }
+
+    return 0;
+}
